b40ac3c960403c390fcac28aa35fba222e5a8c42
Johnny <johnny.andersson.91@gmail.com>
2024-11-07 12:29:41 +0100

    Added search function

---
+++ SocialMedia/SocialMedia/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Data;
+using SocialMedia.ViewModels;
+
+namespace SocialMedia.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public UsersController(ApplicationDbContext dbContext)
+        {
+               _dbcontext = dbContext;  
+        }
+
+        public async Task<IActionResult> Index(UsersIndexViewModel viewModel)
+        {
+            if (viewModel.Search != null)
+            { 
+                var users = await _dbcontext.Users.Where(u => u.Name.Contains(viewModel.Search)).ToListAsync();
+               
+                viewModel.Result = users;
+            }
+         
+            
+           
+            return View(viewModel);
+        }
+    }
+}
---
+++ SocialMedia/SocialMedia/ViewModels/UsersIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SocialMedia.Models;
+
+namespace SocialMedia.ViewModels
+{
+    public class UsersIndexViewModel
+    {
+        public string Search { get; set; }
+        public List<ApplicationUser> Result { get; set; } = new List<ApplicationUser>();
+    }
+}
