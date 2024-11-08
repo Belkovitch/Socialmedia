acda778cb6e436f3422c791b9e2e0250b6fc6b92
Johnny <johnny.andersson.91@gmail.com>
2024-11-08 09:39:38 +0100

    Added show Posts/broadcasts

---
+++ SocialMedia/SocialMedia/ViewModels/UsersShowUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿using SocialMedia.Models;
+
+namespace SocialMedia.ViewModels
+{
+    public class UsersShowUserViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<Broadcast> Broadcasts { get; set; }
+    }
+}
